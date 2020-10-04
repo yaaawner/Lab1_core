9729d146a8e0d27e80f98b46708f3f66af92637b
Olya <belyaevaolga881@gmail.com>
2020-10-05 00:29:32 +0300

    v2collection

--- Lab1_core/Program.cs
+++ Lab1_core/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Lab1_core
 {
@@ -6,16 +7,90 @@ namespace Lab1_core
     {
         struct DataItem
         {
-            //public int x;
-            //public int y;
-            System.Numerics.Vector2 Vec { get; set; }
+            public Vector2 Vector { get; set; }
+            public Complex Complex { get; set; }
+
+            public DataItem (Vector2 vector, Complex complex)
+            {
+                Vector = vector;
+                Complex = complex;
+            }
+
+            public override string ToString()
+            {
+                return "Vector: " + Vector.X.ToString() + " " + Vector.Y.ToString() + "\n" +
+                       "Complex: " + Complex.Real.ToString() + " + " + Complex.Imaginary.ToString() + "i\n";
+            }
         }
 
-        struct Grid1D { }
+        struct Grid1D
+        {
+            public float Step { get; set; }
+            public int Num { get; set; }
 
-        class V2Data { }
+            public Grid1D(float step, int num)
+            {
+                Step = step;
+                Num = num;
+            }
 
-        class V2DataOnGrid : V2Data { }
+            public override string ToString()
+            {
+                return "Step: " + Step.ToString() + "; Num: " + Num.ToString();
+            }
+        }
+
+        abstract class V2Data
+        {
+            public string Info { get; set; }
+            public double Freq { get; set; }
+
+            public V2Data (string info, double freq)
+            {
+                Info = info;
+                Freq = freq;
+            }
+
+            public abstract Complex[] NearAverage(float eps);
+            public abstract string ToLongString();
+            
+            //ToString
+
+        }
+
+        class V2DataOnGrid : V2Data
+        {
+            public Grid1D[] Ox { get; set; }
+            public Grid1D[] Oy { get; set; }
+            public Complex[,] Node { get; set; }
+
+            public V2DataOnGrid (Grid1D[] ox, Grid1D[] oy, string info, double freq) : base(info, freq)
+            {
+                Info = info;
+                Freq = freq;
+                Ox = new Grid1D[ox.Length];
+                Ox = (Grid1D[])ox.Clone();              //!!!
+
+                Oy = new Grid1D[oy.Length];
+                Oy = (Grid1D[])oy.Clone();              //!!!
+            }
+
+            public void initRandom(double minValue, double maxValue)
+            {
+                Node = new Complex[Ox.Length, Oy.Length];
+                Random rnd = new Random();
+                
+                for (int i = 0; i < Ox.Length; i++)
+                {
+                    for (int j = 0; j < Oy.Length; j++)
+                    {
+                        Node[i, j] = rnd.NextDouble() * (maxValue - minValue);
+                    }
+                }
+            } 
+
+            //V2DataCollection
+        }
 
         class V2DataCollection : V2Data { }
 
