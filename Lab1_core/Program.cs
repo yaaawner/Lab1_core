422afc66a4ea89bbb47a5984278dd28caac4ae36
Olya <belyaevaolga881@gmail.com>
2020-10-04 22:55:50 +0300

    Добавьте файлы проекта.

---
+++ Lab1_core/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab1_core
+{
+    class Program
+    {
+        struct DataItem
+        {
+            //public int x;
+            //public int y;
+            System.Numerics.Vector2 Vec { get; set; }
+        }
+
+        struct Grid1D { }
+
+        class V2Data { }
+
+        class V2DataOnGrid : V2Data { }
+
+        class V2DataCollection : V2Data { }
+
+        class V2MainCollection { }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
